8b3e01d376b121f9a664d3628cf3deb2ed9fb4e0
mfuteral <mfuteral@wifi-pro-82-089.paris.inria.fr>
2023-01-26 19:17:32 +0100

    update eval scripts

--- scripts/eval/eval_meteor.sh
+++ scripts/eval/eval_meteor.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
+METEOR_FILE="/PATH/TO/METEOR/"
+REFERENCE_PATH="/PATH/TO/REF/FILE"
+HYPOTHESIS_PATH="/PATH/TO/HYP/FILE"
+
 meteor_eval() {
-    java -Xmx2G -jar ../../../../../meteor-1.5/meteor-1.5.jar "$1" "$2" -l fr > best_model_meteor_score.log
+    java -Xmx2G -jar ${METEOR_FILE} "$1" "$2" -l fr > meteor_score.log
 }
 
-cd $STORE/guided_sa/en-fr
-
-path="MBART_mix_MMT_VMLM_en-fr_Multi30k_from_NMT_adapters_guidedSA_textparamsfrozen_MDETR_mix50_PMask25"
-
-for dir in ${path}/*; do
-	if [ -d ${dir} ]; then
-		echo ${dir}
-		cd ${dir}
-		cd hypothesis_test_2017_flickr; meteor_eval ref.en-fr.test_2017_flickr* hyp*
-		cd ../hypothesis_test_2017_mscoco; meteor_eval ref.en-fr.test_2017_mscoco* hyp*
-		cd ../hypothesis; meteor_eval ref.en-fr.test_2016_flickr* *test_2016_flickr.txt
-		cd ../../..;
-	fi
-done
-wait
+meteor_eval ${REFERENCE_PATH} ${HYPOTHESIS_PATH}
